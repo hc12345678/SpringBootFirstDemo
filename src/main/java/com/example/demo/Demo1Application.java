dcb3c301fbbc168963788e47e3b6d6902fec158a
Hu Min <hu.min@vpclub.cn>
2019-06-04 17:24:31 +0800

    解决冲突

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class Demo1Application {
 
 	public static void main(String[] args) {  
-		SpringApplication.run(Demo1Application.class, args);  
+		SpringApplication.run(Demo1Application.class, args);   
 	}
 
 }
